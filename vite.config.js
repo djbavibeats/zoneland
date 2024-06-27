1bba68d15ebec0921e8f8b42b51f4bd8047b1d6c
Justin <justin@voltcreative.com>
2024-06-27 15:05:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mixPlugin from 'vite-plugin-mix'
-const mix = mixPlugin.default
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
-  base: "./",
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: './src/main.jsx',
-    },
-  },
   plugins: [
     react(),
   ]
